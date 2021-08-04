8f284a8e8509e771d54a653592f612f2636d9748
Eladio Mejias <eladioj6@gmail.com>
2021-08-04 18:19:11 -0400

    Added DatabaseContext

---
+++ MVCASP/MVCASP/Data/ApplicationDBContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVCASP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCASP.Data
+{
+    // Agregando DB Context desde el EF
+    public class ApplicationDBContext: DbContext
+    {
+        // Constructor y pasando a la clase base
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options){ 
+
+        }
+
+        public DbSet<Person> People { get; set; }
+    }
+}
--- MVCASP/MVCASP/Startup.cs
+++ MVCASP/MVCASP/Startup.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MVCASP.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,6 +25,10 @@ namespace MVCASP
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Agregando servicios de DB y vinculando options con el JSON file
+            services.AddDbContext<ApplicationDBContext>(options =>
+               options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))
+            );
             services.AddControllersWithViews();
         }
 
