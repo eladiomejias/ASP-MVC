d66c56eac355f12b368904b56e3a02668a01a852
Eladio Mejias <eladioj6@gmail.com>
2021-08-04 19:37:52 -0400

    Added Connection to SQL Server and retrieve info

---
+++ MVCASP/MVCASP/Controllers/ItemController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCASP.Data;
+using MVCASP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCASP.Controllers
+{
+    public class ItemController : Controller
+    {
+        // Creacion de variable de tipo ApplicationDbContext
+        private readonly ApplicationDBContext _db;
+
+        public ItemController(ApplicationDBContext db)
+        {
+            // Dependy injection en constructor
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Person> objList = _db.People;
+            return View(objList);
+        }
+    }
+}
